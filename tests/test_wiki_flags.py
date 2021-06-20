421c2fbb0f3ae952493a989e305313609523743f
AlDacMac <alasdairmacgdev@gmail.com>
2021-06-20 18:47:43 +0100

    Revert "add predefined flag of scotland page object to wiki_flags tests and modify tests to use it"
    This reverts commit 84fba307584d1a9dc8c93019ef57e1f1b59c55e3.

--- tests/test_wiki_flags.py
+++ tests/test_wiki_flags.py
@@ -1,16 +1,9 @@
 from flaggart.wiki_flags import selectflagpage
 from flaggart.wiki_flags import getpage
 import pytest
-import wikipedia
-
-scotpage = wikipedia.page("flag of scotland")
-
-#Tests for getpage
 
 def test_getpage_basic():
-    assert getpage("Scotland") == scotpage
-
-#Tests for selectpage
+    assert getpage("Scotland").title == "Flag of Scotland"
 
 def test_selectpage_basic():
     assert selectflagpage("Scotland", ['Flag of Scotland']) == 'Flag of Scotland'
